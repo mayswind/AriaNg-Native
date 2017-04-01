a39dac37102cc1b3746a360cb95177f8e491c7b0
MaysWind <i@mayswind.net>
2017-04-02 00:58:02 +0800

    package AriaNg Native in windows

---
+++ app.js
@@ -0,0 +1,34 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+const Tray = electron.Tray;
+
+var mainWindow = null;
+var tray = null;
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({
+        title: 'AriaNg',
+        width: 1000,
+        height: 600,
+        minWidth: 400,
+        minHeight: 400,
+        webPreferences: {
+            nodeIntegration: false
+        }
+    });
+
+    mainWindow.setMenu(null);
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+});
