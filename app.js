981bc822786865fbc2bcfeb995f7c61fb46012e5
MaysWind <i@mayswind.net>
2018-02-23 21:35:37 +0800

    update third party dependencies and fix shortcut bug in macos

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const os = require('os');
 const electron = require('electron');
+const electronLocalshortcut = require('electron-localshortcut');
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
@@ -22,6 +25,32 @@ app.on('ready', function () {
             nodeIntegration: false
         }
     });
+
+    if (os.platform() == 'darwin') {
+        electronLocalshortcut.register(mainWindow, 'CmdOrCtrl+Z', function () {
+            mainWindow.webContents.undo();
+        });
+
+        electronLocalshortcut.register(mainWindow, 'Shift+CmdOrCtrl+Z', function () {
+            mainWindow.webContents.redo();
+        });
+
+        electronLocalshortcut.register(mainWindow, 'CmdOrCtrl+X', function () {
+            mainWindow.webContents.cut();
+        });
+
+        electronLocalshortcut.register(mainWindow, 'CmdOrCtrl+C', function () {
+            mainWindow.webContents.copy();
+        });
+
+        electronLocalshortcut.register(mainWindow, 'CmdOrCtrl+V', function () {
+            mainWindow.webContents.paste();
+        });
+
+        electronLocalshortcut.register(mainWindow, 'CmdOrCtrl+A', function () {
+            mainWindow.webContents.selectAll();
+        });
+    }
 
     mainWindow.setMenu(null);
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
