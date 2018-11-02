704b92e936d26281d394a760337bd623349464b2
kisaihou <382204713@qq.com>
2018-11-02 11:36:28 +0800

    Add binaries for standalone

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const os = require('os');
 const electron = require('electron');
 const electronLocalshortcut = require('electron-localshortcut');
 
@@ -7,14 +6,45 @@ const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 const Tray = electron.Tray;
 
-var mainWindow = null;
-var tray = null;
+const exec = require('child_process').exec;
+
+
+let cmdStr = 'aria2c --conf-path=./aria2.conf';
+let cmdPath = './assets/aria2/';
+let aria2Process;
+let processes = [];
+
+let mainWindow = null;
+let tray = null;
+
+function runAria2() {
+    if (process.platform === 'darwin') {
+        cmdPath += 'osx/';
+    } else if (process.platform === 'linux') {
+        //forserver
+    } else if (process.platform === 'win32') {
+        if (process.arch === 'x64') {
+            cmdPath += 'win64/';
+        } else {
+            cmdPath += 'win32/';
+        }
+        aria2Process = exec(cmdStr, {cwd: cmdPath});
+    }
+    processes.push(aria2Process);
+    aria2Process.on("exit", function () {
+        processes.splice(processes.indexOf(aria2Process), 1);
+    });
+}
 
 app.on('window-all-closed', function () {
+    processes.forEach(function (proc) {
+        proc.kill();
+    });
     app.quit();
 });
 
 app.on('ready', function () {
+    runAria2();
     mainWindow = new BrowserWindow({
         title: 'AriaNg',
         width: 1000,
@@ -26,8 +56,7 @@ app.on('ready', function () {
             nodeIntegration: false
         }
     });
-
-    if (os.platform() == 'darwin') {
+    if (process.platform == 'darwin') {
         electronLocalshortcut.register(mainWindow, 'CmdOrCtrl+Z', function () {
             mainWindow.webContents.undo();
         });
@@ -64,3 +93,5 @@ app.on('ready', function () {
         mainWindow = null;
     });
 });
+
+
